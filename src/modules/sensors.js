a76312144c5b1e270f127645598a17ba1e94a008
Salva <ssantamaria@institutmvm.cat>
2026-05-22 10:29:19 +0200

    test: prueba para vercel

--- src/modules/sensors.js
+++ src/modules/sensors.js
@@ -10,6 +10,7 @@ const sensorState = {
   active:         false,
   accel:          { x: 0, y: 0, z: 0 },
   orientation:    { alpha: 0, beta: 0, gamma: 0 },
+  location:       { latitude: 0, longitude: 0},
   shakeThreshold: 18,
   lastShakeTime:  0,
 };
